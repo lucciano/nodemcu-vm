a079dd89e9bd56a3a9c82dcc42fa3ce3012e84ed
Luciano Andrade <andrade.luciano@gmail.com>
2017-01-05 22:01:45 -0300

    ...

--- Vagrantfile
+++ Vagrantfile
@@ -68,6 +68,6 @@ Vagrant.configure("2") do |config|
      apt-get update
      apt-get upgrade -yq
      apt-get install -yq curl
-     sudo -u ubuntu curl https://raw.githubusercontent.com/lucciano/nodemcu-vm/master/nodemcu.sh | sh
+     curl https://raw.githubusercontent.com/lucciano/nodemcu-vm/master/nodemcu.sh | sudo -u ubuntu sh
   SHELL
 end
--- nodemcu.sh
+++ nodemcu.sh
@@ -4,11 +4,10 @@ sudo apt-get upgrade -yq
 sudo apt-get install -yq build-essential
 sudo apt-get install -yq make unrar-free autoconf automake libtool gcc g++ gperf \
 	flex bison texinfo gawk ncurses-dev libexpat-dev python-dev python \
-	python-serial sed git unzip bash help2man wget bzip2
+	python-serial sed git unzip bash help2man wget bzip2 libtool-bin
 
-sudo apt-get install libtool-bin
 git clone https://github.com/pfalcon/esp-open-sdk.git ~/esp-open-sdk
-cd /opt/esp-open-sdk
+cd ~/esp-open-sdk
 make
 export PATH=/opt/esp-open-sdk/xtensa-lx106-elf/bin:$PATH
 git clone https://github.com/nodemcu/nodemcu-firmware.git ~/nodemcu-firmware
